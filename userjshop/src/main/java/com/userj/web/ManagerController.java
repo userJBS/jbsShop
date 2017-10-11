3f1bb49b6be36045f3c9d7960913cdf3a075e4b4
jbs <jbs@192.168.123.102>
2017-10-11 18:57:26 +0900

    [add][관리자 페이지 주무 리스트 추가~]

--- userjshop/src/main/java/com/userj/web/ManagerController.java
+++ userjshop/src/main/java/com/userj/web/ManagerController.java
@@ -7,19 +7,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.userj.repository.AccountRepository;
+import com.userj.repository.OrderRepository;
 
-//  (관리 페이지) 홈페이지 관리자 페이지 
+//  관리자만 볼수 있는 페이지
 @Controller
-@RequestMapping("/account")
+@RequestMapping("/account/manager")
 public class ManagerController {
 	@Autowired
 	private AccountRepository accountRepository;
-
-	// 관리자만 볼수 있는 페이지 모든 회원 리스트 (관리)
-	@GetMapping("/manager")
+	@Autowired
+	private OrderRepository orderRepository;
+	// 모든 회원 리스트 (관리)
+	@GetMapping("/list")
 	public String list(Model model) {
 		model.addAttribute("account", accountRepository.findAll());
 		return "/account/manager/list";
+	}
+
+	// 주문 리스트(회원들이 주문한 상품)
+	@GetMapping("/order")
+	public String productList(Model model) {
+		model.addAttribute("order", orderRepository.findAll());
+		 
+		
+		return "/account/manager/order";
 	}
 
 }
